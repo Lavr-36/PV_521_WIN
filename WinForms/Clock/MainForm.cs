d40d8e27d190cb3d5a3931cc2cf206ea344f0e92
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2025-12-24 20:00:51 +0300

    Colors DONE 2

--- WinForms/Clock/MainForm.cs
+++ WinForms/Clock/MainForm.cs
@@ -10,100 +10,99 @@ using System.Windows.Forms;
 
 namespace Clock
 {
-	public partial class MainForm : Form
-	{
-		ColorDialog foregroundColorDialog;
-		ColorDialog backgroundColorDialog;
-		public MainForm()
-		{
+    public partial class MainForm : Form
+    {
+        ColorDialog foregroundColorDialog;
+        ColorDialog backgroundColorDialog;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point
+                (
+                    Screen.PrimaryScreen.Bounds.Width - this.Width,
+                    50
+                );
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            SetVisibility(false);
+            foregroundColorDialog = new ColorDialog();
+            backgroundColorDialog = new ColorDialog();
+        }
+        void SetVisibility(bool visible)
+        {
+            cbShowDate.Visible = visible;
+            cbShowWeekday.Visible = visible;
+            btnHideControls.Visible = visible;
+            this.ShowInTaskbar = visible;
+            this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+            this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString
+                (
+                "hh:mm:ss tt",
+                System.Globalization.CultureInfo.InvariantCulture
+                );
+            if (cbShowDate.Checked)
+                labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+            if (cbShowWeekday.Checked)
+                labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
 
-			InitializeComponent();
-			this.StartPosition = FormStartPosition.Manual;
-			this.Location = new Point
-				(
-				Screen.PrimaryScreen.Bounds.Width - this.Width - 25,
-				50
-				);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			SetVisibility(false);
-			foregroundColorDialog = new ColorDialog();
-			backgroundColorDialog = new ColorDialog();
-		}
-		void SetVisibility(bool visible)
-		{
-			cbShowDate.Visible = visible;
-			cbShowWeekday.Visible = visible;
-			btnHideControls.Visible = visible;
-			this.ShowInTaskbar = visible;
-			this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
-			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
-		}
-		private void timer_Tick(object sender, EventArgs e)
-		{
-			labelTime.Text = DateTime.Now.ToString
-				(
-				"hh:mm:ss tt",
-				System.Globalization.CultureInfo.InvariantCulture
-				);
-			if (cbShowDate.Checked)
-				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
-			if (cbShowWeekday.Checked)
-				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+            notifyIcon.Text = labelTime.Text;
+        }
 
-			notifyIcon.Text = labelTime.Text;
-		}
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+            SetVisibility(tsmiShowControls.Checked = false);
+        }
 
-		private void btnHideControls_Click(object sender, EventArgs e)
-		{
-			SetVisibility(tsmiShowControls.Checked = false);
-		}
+        //private void labelTime_MouseHover(object sender, EventArgs e)
+        //{
+        //	SetVisibility(true);
+        //}
 
-		//private void labelTime_MouseHover(object sender, EventArgs e)
-		//{
-		//	SetVisibility(true);
-		//}
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            if (!TopMost)
+            {
+                this.TopMost = true;
+                this.TopMost = false;
+            }
+        }
 
-		private void notifyIcon_DoubleClick(object sender, EventArgs e)
-		{
-			if (!TopMost)
-			{
-				this.TopMost = true;
-				this.TopMost = false;
-			}
-		}
+        private void tsmiTopmost_Click(object sender, EventArgs e) => this.TopMost = tsmiTopmost.Checked;
 
-		private void tsmiTopmost_Click(object sender, EventArgs e) => this.TopMost = tsmiTopmost.Checked;
+        private void tsmiShowControls_CheckedChanged(object sender, EventArgs e)
+        {
+            SetVisibility((sender as ToolStripMenuItem).Checked);
+            //Sender - это отправитель события (Control, который прислал событие).
+            //Если на элемент окна (Control) воздействует пользователь при помощи клавиатуры или мыши,
+            //этот Control отправляет событие свему родителю,
+            //а родитель может обраьатывать, или не обрабатывать это событие.
+        }
 
-		private void tsmiShowControls_CheckedChanged(object sender, EventArgs e)
-		{
-			SetVisibility((sender as ToolStripMenuItem).Checked);
-			//Sender - это отправитель события (Control, который прислал событие).
-			//Если на элемент окна (Control) воздействует пользователь при помощи клавиатуры или мыши,
-			//этот Control отправляет событие свему родителю,
-			//а родитель может обраьатывать, или не обрабатывать это событие.
-		}
+        private void tsmiShowDate_CheckedChanged(object sender, EventArgs e) => cbShowDate.Checked = tsmiShowDate.Checked;
 
-		private void tsmiShowDate_CheckedChanged(object sender, EventArgs e) => cbShowDate.Checked = tsmiShowDate.Checked;
+        private void cbShowDate_CheckedChanged(object sender, EventArgs e) => tsmiShowDate.Checked = cbShowDate.Checked;
 
-		private void cbShowDate_CheckedChanged(object sender, EventArgs e) => tsmiShowDate.Checked = cbShowDate.Checked;
+        private void tsmiShowWeekday_CheckedChanged(object sender, EventArgs e) => cbShowWeekday.Checked = tsmiShowWeekday.Checked;
 
-		private void tsmiShowWeekday_CheckedChanged(object sender, EventArgs e) => cbShowWeekday.Checked = tsmiShowWeekday.Checked;
+        private void cbShowWeekday_CheckedChanged(object sender, EventArgs e) => tsmiShowWeekday.Checked = cbShowWeekday.Checked;
 
-		private void cbShowWeekday_CheckedChanged(object sender, EventArgs e) => tsmiShowWeekday.Checked = cbShowWeekday.Checked;
-
-		private void tsmiQuit_Click(object sender, EventArgs e) => this.Close();
+        private void tsmiQuit_Click(object sender, EventArgs e) => this.Close();
 
         private void tsmiForegroundColor_Click(object sender, EventArgs e)
         {
             foregroundColorDialog.ShowDialog();
-			labelTime.ForeColor = foregroundColorDialog.Color;
+            labelTime.ForeColor = foregroundColorDialog.Color;
         }
 
         private void tsmiBackgroundColor_Click(object sender, EventArgs e)
         {
-			backgroundColorDialog.ShowDialog();
-			labelTime.BackColor = backgroundColorDialog.Color;
+            backgroundColorDialog.ShowDialog();
+            labelTime.BackColor = backgroundColorDialog.Color;
         }
     }
 }
